cca36c1446731cf1315190ad3908c9bf358cd100
kirushanthSr <kirushanth0611@gmail.com>
2025-05-29 19:53:58 +0530

    files upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'your-repo-name' with the actual name of your GitHub repo
 export default defineConfig({
+  base: 'Portfolio_Kirushnath',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
